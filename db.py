af38b5a74069b325fa21dc94c209f704e9e14df6
Gerald Low <lowbicheng@gmail.com>
2017-09-09 11:08:39 +0800

    new repo

---
+++ db.py
@@ -0,0 +1,75 @@
+#db.py
+import sqlalchemy
+import pandas as pd
+
+class TradeDatabase(object):
+	def __init__(self):
+		pass
+
+	def connect(self, user='postgres', password='pokemon1995', db='tradingbot', host='localhost', port=5432):
+		'''Returns a connection and a metadata object'''
+		# We connect with the help of the PostgreSQL URL
+		self.url = 'postgresql://{}:{}@{}:{}/{}'
+		self.url = self.url.format(user, password, host, port, db)
+
+		# The return value of create_engine() is our connection object
+		self.con = sqlalchemy.create_engine(self.url, client_encoding='utf8')
+
+		# We then bind the connection to MetaData()
+		self.meta = sqlalchemy.MetaData(bind=self.con, reflect=True)
+		#table
+		self.db = self.meta.tables['tick']
+
+	def insertStatement01(self, date, price):
+		insert_statement = self.db.insert().values(date=date,price=price)
+		self.con.connect().execute(insert_statement)
+
+	def insertStatement02(self, date, trend, SMA, RSI):
+		update_statement = self.db.update().where(self.db.c.date==date).values(trend=trend,sma=SMA,rsi=RSI)
+		self.con.connect().execute(update_statement)
+
+	def insertStatement03(self, date, cryptoamount, long, tradePlaced, typeOfTrade):
+		update_statement = self.db.update().where(self.db.c.date==date).values(cryptoamount=cryptoamount,long=long,tradeplaced=tradePlaced,typeoftrade=typeOfTrade)
+		self.con.connect().execute(update_statement)
+
+	def insertStatement04(self, date, cryptoamount, short, tradePlaced, typeOfTrade):
+		update_statement = self.db.update().where(self.db.c.date==date).values(cryptoamount=cryptoamount,short=short,tradeplaced=tradePlaced,typeoftrade=typeOfTrade)
+		self.con.connect().execute(update_statement)
+
+	def insertStatement05(self, date, netProfit):
+		update_statement = self.db.update().where(self.db.c.date==date).values(netProfit=netprofits)
+		self.con.connect().execute(update_statement)
+
+	def insertStatement06(self, date, closedLong, tradePlaced):
+		update_statement = self.db.update().where(self.db.c.date==date).values(closedlong=closedLong,tradeplaced=tradePlaced)
+		self.con.connect().execute(update_statement)
+
+	def insertStatement07(self, date, closedShort, tradePlaced):
+		update_statement = self.db.update().where(self.db.c.date==date).values(closedshort=closedShort,tradeplaced=tradePlaced)
+		self.con.connect().execute(update_statement)
+
+	def frameit(self):
+		self.df = pd.read_sql_table('tick', self.con)
+		return self.df
+
+	def readtolist01(self):
+		self.df = pd.read_sql_table('tick', self.con)
+		self.datelist = self.df['date'].tolist()
+		self.pricelist = self.df['price'].tolist()
+		return self.datelist, self.pricelist
+
+	def readtolist02(self):
+		self.df = pd.read_sql_table('tick', self.con)
+		self.typeoftradelist = self.df['typeoftrade'].tolist()
+		self.tradeplacedlist = self.df['tradeplaced'].tolist()
+		self.cryptoamountlist = self.df['cryptoamount'].tolist()
+		return self.tradeplacedlist, self.typeoftradelist, self.cryptoamountlist
+
+	def cumwinloss(self):
+		self.df = pd.read_sql_table('tick', self.con)
+		self.netprofitslist = self.df['netprofits'].tolist()
+		self.cumulatedprofits = sum(self.netprofitslist)
+		self.numofwins = len([i for i in self.netprofitslist if i > 0])
+		self.numofloses = len([i for i in self.netprofitslist if i < 0])
+		return self.cumulatedprofits, self.numofwins, self.numofloses
+
